19366e8835d39f5c4ea455f9f306fc15d7040281
Jannis Jehmlich <jjehmlich@yahoo.de>
2020-05-20 19:06:42 +0200

    feat: version of train.py ready

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-
 # Imports
 from argparse import ArgumentParser
 import torch
@@ -9,15 +8,15 @@ from collections import OrderedDict
 from PIL import Image
 from numpy import asarray
 import numpy as np
-import train_utils
+from train_utils import *
 
 # Create a parser to handle the passed arguments
 parser = ArgumentParser(description = "Train and save a neural network")
 # add the arguments handled by the parser
 parser.add_argument("data_directory", metavar = "", help = "the directory of the data to be trained and tested on")
-parser.add_argument("-sd", "--save_dir", metavar = "", help = "the directory where the model is saved")
-parser.add_argument("--arch", metavar = "", help = "the pretrained model that should be used for training")
-parser.add_argument("-lr", "--learning_rate", type = float, metavar = "", help = "The learning rate as a float")
+parser.add_argument("-sd", "--save_dir", nargs = "?", default = "checkpoint.pth", metavar = "", help = "the directory and checkpoint name where the model is saved")
+parser.add_argument("--arch", nargs = "?", default = "vgg13",type = str, metavar = "", help = "the pretrained model that should be used for training")
+parser.add_argument("-lr", "--learning_rate",nargs = "?", default = 0.003, type = float, metavar = "", help = "The learning rate as a float")
 
 # function as a new type to handle the boundaries of the hidden units
 def hidden_units_type(x):
@@ -28,13 +27,46 @@ def hidden_units_type(x):
     if x < 102 or x > 25088:
         raise argparse.ArgumentTypeError("Value needs to be between 102 and 25088 units")
     return x
-parser.add_argument("-hu", "--hidden_units", type = hidden_units_type, metavar = "", help = "The number of nodes in the hidden layer")
+parser.add_argument("-hu", "--hidden_units", type = hidden_units_type,nargs = "?", default = 512, metavar = "", help = "The number of nodes in the hidden layer")
 
-parser.add_argument("-e", "--epochs", type = int, metavar = "", help = "The number of epochs used for training (Should be a small number as 1 epoch takes about 5-8 min")
+parser.add_argument("-e", "--epochs", type = int, nargs = "?", default = 1, metavar = "", help = "The number of epochs used for training (Should be a small number as 1 epoch takes about 5-8 min")
 parser.add_argument("-gpu", "--gpu", action = "store_true", help = "Uses the gpu for training if available")
+parser.add_argument("-v", "--verbose", action = "store_true", help = "Prints status messages")
+
 # save the passed arguments in a variable
 args = parser.parse_args()
+v = args.verbose
+verbose = v
+# Load the data
+if v:
+    print("Loading data")
+data_dir = args.data_directory
+train_dir = data_dir + '/train'
+valid_dir = data_dir + '/valid'
+test_dir = data_dir + '/test'
 
+trainloader = train_transforms(train_dir)
+validloader = test_transforms(valid_dir)
+testloader = test_transforms(test_dir)
 
-if __name__ == "__main__":
-    print("The passed Argumens are '{}'".format(args.data_directory))
+# Load mapping dict:
+if v:
+    print("Loading mapping dict")
+cat_to_name = get_cat_to_name()
+
+# load pretrained model
+if v:
+    print("Loading pretrained model {}".format(str(args.arch)))
+model = load_pretrained_nn(args.arch)
+
+# build and attach new classifier
+if v:
+    print("Building new classifier with {} hidden units".format(str(args.hidden_units)))
+model.classifier = build_new_classifier(args.hidden_units)
+
+# train the model
+optimizer = optim.Adam(model.classifier.parameters(), float(args.learning_rate))
+model, optimizer = train_model(model,optimizer, trainloader, validloader, args.epochs, args.gpu, verbose)
+
+# save the model
+save_model(args.arch, model, optimizer, cat_to_name, train_data.class_to_idx, args.save_dir)
--- train_utils.py
+++ train_utils.py
@@ -1,3 +1,15 @@
+# Imports
+from argparse import ArgumentParser
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+from collections import OrderedDict
+from PIL import Image
+from numpy import asarray
+import numpy as np
+
+
 def train_transforms(train_dir):
     '''
     Takes the directory of the training data as an input and returns a trainloader with images transformed for training.
@@ -42,16 +54,102 @@ def load_pretrained_nn(model_name):
     '''
     Takes the model architecture as a string input and returns the loaded and pretrained model with all parameters frozen.
     '''
-    model = getattr(models, model_name)(pretrained = True)
+    # get the requested model
+    model = getattr(models, str(model_name))(pretrained = True)
+    # freeze parameters of pretrained model
     for param in model.parameters():
         param.requires_grad = False
     return model
 
 def build_new_classifier(hidden_units):
+    '''
+    Builds a new classifier with the requested amount of hidden units.
+    '''
     classifier = nn.Sequential(
                            nn.Linear(25088, hidden_units),
                            nn.ReLU(inplace = True),
                            nn.Dropout(p=0.2),
                            nn.Linear(hidden_units, 102),
                            nn.LogSoftmax(dim = 1))
-    model.classifier = classifier
+    return classifier
+
+def train_model(model, optimizer, trainloader, validloader, epoch, gpu, verbose):
+    '''
+    Trains the classifier attached to the pretrained model.
+    Inputs:
+    model - pretrained neural network
+    optimizer - optimizer objekt
+    trainloader - object to supply the training images
+    validloader - object to supply the validation images
+    epoch - int - the number of epochs to be run
+    gpu - Bool - If the gpu should be used if possible
+
+    '''
+    criterion = nn.NLLLoss()
+
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if gpu:
+        model.to(device)
+        if device == "gpu":
+            print("GPU training activated")
+
+    model.train()
+    epochs = epoch
+    steps = 0
+    print_every = 5
+    running_loss = 0
+
+
+    for epoch in range(epochs):
+        print("Training in epoch {}".format(str(epoch)))
+        for images, labels in trainloader:
+            steps +=1
+            if gpu:
+                images, labels = images.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+            logps = model(images)
+            loss = criterion(logps, labels)
+
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if steps % print_every == 0:
+                if verbose:
+                    print("Current batch: {}".format(str(steps)))
+                valid_loss = 0
+                accuracy = 0
+                model.eval()
+
+                with torch.no_grad():
+                    for images, labels in validloader:
+                        if gpu:
+                            images, labels = images.to(device), labels.to(device)
+                        logps = model(images)
+
+                        valid_loss += criterion(logps, labels).item()
+
+                        #accuracy
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim = 1)
+                        equals = top_class == labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor))
+
+                    print("Validation loss: {}".format(valid_loss/len(validloader)))
+                    print("Validation Accuracy: {}".format(accuracy/len(validloader)))
+                    model.train()
+            print("Train loss: {}".format(running_loss/len(trainloader)))
+    return model, optimizer
+
+def save_model(arch, model, optimizer, cat_to_name, class_to_idx, save_dir):
+    checkpoint = {"transfer_model": arch,
+             "classifier": model.classifier,
+             "classifier_state_dict": model.classifier.state_dict(),
+             "optimizer": optimizer,
+             "optimizer_state_dict": optimizer.state_dict(),
+             "class_to_idx": class_to_idx,
+             "class_to_name": model.class_to_name}
+    torch.save(checkpoint, save_dir)
